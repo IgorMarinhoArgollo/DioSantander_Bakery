1eb90825cd253c54d14b205daca1cf7cb86ed6ab
IgorMarinhoArgollo <igormarinhoargollo@gmail.com>
2023-10-22 18:07:07 -0300

    feat: fixing cors

--- src/main/java/dioBootcamp/bakery/Application.java
+++ src/main/java/dioBootcamp/bakery/Application.java
@@ -1,8 +1,11 @@
 package dioBootcamp.bakery;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default server URL")}) // cors  solving
 @SpringBootApplication
 public class Application {
 
